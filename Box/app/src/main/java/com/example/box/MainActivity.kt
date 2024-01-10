66066b477dbe49366334489feab46656a2f9b867
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-01-10 15:05:59 -0300

    studying box in jetpack compose

--- Box/app/src/main/java/com/example/box/MainActivity.kt
+++ Box/app/src/main/java/com/example/box/MainActivity.kt
@@ -4,18 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.box.ui.theme.BoxTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,6 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    Greeting()
                 }
             }
         }
@@ -36,13 +38,50 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting() {
-    Box(
-        modifier = Modifier.background(Color.Blue)
-            .width(100.dp)
-            .height(100.dp)
-    ) {
+  Box(
+      modifier = Modifier.fillMaxSize(),
+      contentAlignment = Alignment.TopCenter
+  ){
+      Box(
+          modifier = Modifier
+              .background(Color.Blue)
+              .width(100.dp)
+              .height(100.dp)
+              .verticalScroll(rememberScrollState())
+      ){
+          Text(
+              text = "I'm Guga Padilha",
+              fontSize = 40.sp,
+              color = Color.White
+              )
+      }
+  }
 
+}
+
+@Composable
+fun GreetingTest2() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopCenter
+    ){
+        Box(
+            modifier = Modifier
+                .background(Color.Blue),
+            contentAlignment = Alignment.CenterEnd
+        ){
+            Box(modifier = Modifier
+                .height(50.dp)
+                .width(50.dp)
+                .background(Color.Green))
+            Text(
+                text = "I'm Guga Padilha",
+                fontSize = 40.sp,
+                color = Color.White
+            )
+        }
     }
+
 }
 
 @Preview(showBackground = true)
@@ -50,5 +89,13 @@ fun Greeting() {
 fun DefaultPreview() {
     BoxTheme {
         Greeting()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreviewTest2() {
+    BoxTheme {
+        GreetingTest2()
     }
 }
