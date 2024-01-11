ef7b64e1521cf51ffacb11ac92d65b88b2a7b215
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-01-11 14:29:40 -0300

    starting learn text costumazation

--- TextCustomization/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ TextCustomization/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -22,22 +23,20 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    DefaultPreview()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+       Column(
+           modifier = Modifier.fillMaxSize()){
+               Text(text = "Hello World")
+           }
     }
 }
