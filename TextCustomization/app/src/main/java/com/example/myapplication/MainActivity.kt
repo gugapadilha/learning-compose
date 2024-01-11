93c8ffca8faa567b977986307b676fc556d5b193
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-01-11 15:56:53 -0300

    string resource in jetpack compose

--- TextCustomization/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ TextCustomization/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,7 +40,12 @@ fun DefaultPreview() {
     MyApplicationTheme {
        Column(
            modifier = Modifier.fillMaxSize()){
-               Text(text = "Hello World")
+               Text(
+                   text = stringResource(id = R.string.app_name),
+                   modifier = Modifier
+                       .background(MaterialTheme.colors.primary) //if i put this background after padding that will not paint the padding
+                       .padding(16.dp)
+               )
            }
     }
 }
