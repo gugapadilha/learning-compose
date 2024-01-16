049901f0343118a1c12d81f77d14d63a296471cc
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-01-16 15:45:54 -0300

    creating first text

--- ExpandableCard/app/src/main/java/com/example/expandablecard/ExpandableCard.kt
+++ ExpandableCard/app/src/main/java/com/example/expandablecard/ExpandableCard.kt
@@ -3,11 +3,19 @@ package com.example.expandablecard
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.expandablecard.ui.theme.Shapes
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -29,6 +37,24 @@ fun ExpandableCard(){
             expandedState = !expandedState
         }
     ) {
-
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp)
+        ) {
+            Row {
+                Text(
+                    text = "My Title",
+                    fontSize = MaterialTheme.typography.h6.fontSize,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+        }
     }
+}
+
+@Composable
+@Preview
+fun ExpandableCardPreview(){
+    ExpandableCard()
 }
