d3496252acfa7e27706bd3913ead5085061db8b8
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-01-16 15:42:13 -0300

    creating the shape of the card

--- ExpandableCard/app/src/main/java/com/example/expandablecard/ExpandableCard.kt
+++ ExpandableCard/app/src/main/java/com/example/expandablecard/ExpandableCard.kt
@@ -1,4 +1,34 @@
 package com.example.expandablecard
 
-class ExpandableCard {
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import com.example.expandablecard.ui.theme.Shapes
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun ExpandableCard(){
+    var expandedState by remember { mutableStateOf(false) }
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .animateContentSize(
+                animationSpec = tween(
+                    delayMillis = 300,
+                    easing = LinearOutSlowInEasing
+                )
+            ),
+        shape = Shapes.medium,
+        onClick = {
+            expandedState = !expandedState
+        }
+    ) {
+
+    }
 }
