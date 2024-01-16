300773480f071ca340b5af8644159c3c12072513
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-01-16 16:04:56 -0300

    card preview done, click working as well

--- ExpandableCard/app/src/main/java/com/example/expandablecard/ExpandableCard.kt
+++ ExpandableCard/app/src/main/java/com/example/expandablecard/ExpandableCard.kt
@@ -2,6 +2,7 @@ package com.example.expandablecard
 
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,7 +15,9 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.expandablecard.ui.theme.Shapes
@@ -23,13 +26,15 @@ import com.example.expandablecard.ui.theme.Shapes
 @Composable
 fun ExpandableCard(){
     var expandedState by remember { mutableStateOf(false) }
+    val rotationState by animateFloatAsState(
+        targetValue = if(expandedState) 180f else 0f)
 
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .animateContentSize(
                 animationSpec = tween(
-                    delayMillis = 300,
+                    durationMillis = 300,
                     easing = LinearOutSlowInEasing
                 )
             ),
@@ -50,19 +55,35 @@ fun ExpandableCard(){
                     text = "My Title",
                     fontSize = MaterialTheme.typography.h6.fontSize,
                     fontWeight = FontWeight.Bold,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
                 )
                 IconButton(
                     modifier = Modifier
                         .alpha(ContentAlpha.medium)
-                        .weight(1f) /* so, icon button will only use 1f of the total 6f */,
+                        .weight(1f) /* so, icon button will only use 1f of the total 7f */
+                        .rotate(rotationState),
                     onClick = {
                         expandedState = !expandedState
                     }
                 ) {
                     Icon(
                         imageVector = Icons.Default.ArrowDropDown,
-                        contentDescription = "Drop-Down Arrow" )
+                        contentDescription = "Drop-Down Arrow"
+                    )
                 }
+            }
+            if (expandedState){
+                Text(
+                    text = "Lorem ipsum dolor sit amet, consectur adispcing elit," +
+                            "sed do eismow temport incidunt ut labore rt magna ali" +
+                            "enim ad minim venuiiam quis nostrud exercitation lamma" +
+                            "laboris nisi ut alinguioe ex ea commodod consqueat des",
+                    fontSize = MaterialTheme.typography.subtitle1.fontSize,
+                    fontWeight = FontWeight.Normal,
+                    maxLines = 4,
+                    overflow = TextOverflow.Ellipsis
+                )
             }
         }
     }
