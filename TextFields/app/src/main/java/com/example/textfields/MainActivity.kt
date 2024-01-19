b98bb9ce16b51b667011b892d62c9e9533fd18fa
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-01-19 11:14:39 -0300

    adding ime option to go

--- TextFields/app/src/main/java/com/example/textfields/MainActivity.kt
+++ TextFields/app/src/main/java/com/example/textfields/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.textfields
 
+import android.inputmethodservice.Keyboard
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -7,6 +8,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
@@ -14,6 +16,8 @@ import androidx.compose.material.icons.filled.Email
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.textfields.ui.theme.TextFieldsTheme
 
@@ -63,7 +67,12 @@ fun Greeting() {
                 imageVector = Icons.Filled.Check,
                 contentDescription = "Check Icon"
             )
-        })
+        },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Email,
+            imeAction = ImeAction.Go
+        )
+        )
     }
 }
 
