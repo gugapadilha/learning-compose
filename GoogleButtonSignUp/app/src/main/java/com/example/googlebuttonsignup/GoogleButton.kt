0ed5d861896a4b8268289c898a2751647f10171d
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-01-22 14:21:43 -0300

    styling icon e text to fit material compose default valuer

--- GoogleButtonSignUp/app/src/main/java/com/example/googlebuttonsignup/GoogleButton.kt
+++ GoogleButtonSignUp/app/src/main/java/com/example/googlebuttonsignup/GoogleButton.kt
@@ -1,13 +1,15 @@
 package com.example.googlebuttonsignup
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -24,10 +26,21 @@ fun GoogleButton(){
         border = BorderStroke(width = 1.dp, color = Color.LightGray),
         color = MaterialTheme.colors.surface
     ) {
-        Row {
+        Row(
+            modifier = Modifier
+                .padding(
+                    start = 12.dp,
+                    end = 16.dp,
+                    top = 12.dp,
+                    bottom = 12.dp
+                ),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
             Icon(painter = painterResource(id = R.drawable.ic_google_logo),
                 contentDescription = "Google Button",
                 tint = Color.Unspecified)
+            Spacer(modifier = Modifier.width(8.dp))
             Text(text = "Sign up with Google")
         }
     }
