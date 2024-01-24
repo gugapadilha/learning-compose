e878435e62198fad44102cc575c63d6cf8d1b98e
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-01-24 11:28:13 -0300

    photo showing successfully

--- ImageLoadingCoil/app/src/main/java/com/example/imageloadingcoil/MainActivity.kt
+++ ImageLoadingCoil/app/src/main/java/com/example/imageloadingcoil/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.imageloadingcoil
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.example.imageloadingcoil.ui.theme.ImageLoadingCoilTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    CoilImage()
                 }
             }
         }
@@ -30,14 +37,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CoilImage() {
+    Box(
+        modifier = Modifier
+            .height(150.dp)
+            .width(150.dp),
+    contentAlignment = Alignment.Center)
+    {
+        val painter = rememberImagePainter(data = "https://avatars.githubusercontent.com/u/14994036?v=4",
+        builder = {
+
+        })
+        Image(painter = painter, contentDescription = "Android Picture")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ImageLoadingCoilTheme {
-        Greeting("Android")
+        CoilImage()
     }
 }
