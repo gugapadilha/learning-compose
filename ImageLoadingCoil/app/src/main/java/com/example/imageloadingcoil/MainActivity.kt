18db63571f376b863c72878cba6752cd510750c3
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-01-24 11:40:05 -0300

    loading state when loading image, and error when image doesnt exist

--- ImageLoadingCoil/app/src/main/java/com/example/imageloadingcoil/MainActivity.kt
+++ ImageLoadingCoil/app/src/main/java/com/example/imageloadingcoil/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -16,6 +17,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.ImagePainter
 import coil.compose.rememberImagePainter
 import com.example.imageloadingcoil.ui.theme.ImageLoadingCoilTheme
 
@@ -36,6 +39,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalCoilApi::class)
 @Composable
 fun CoilImage() {
     Box(
@@ -44,11 +48,17 @@ fun CoilImage() {
             .width(150.dp),
     contentAlignment = Alignment.Center)
     {
-        val painter = rememberImagePainter(data = "https://avatars.githubusercontent.com/u/14994036?v=4",
+        val painter = rememberImagePainter(data = "https://avatars.githubusercntent.com/u/14994036?v=4",
         builder = {
-
+            placeholder(R.drawable.baseline_image_24)
+            error(R.drawable.baseline_error_24)
+            crossfade(1000)
         })
+        val painterState = painter.state
         Image(painter = painter, contentDescription = "Android Picture")
+        //if (painterState is ImagePainter.State.Loading){
+          //  CircularProgressIndicator()
+        //}
     }
 }
 
