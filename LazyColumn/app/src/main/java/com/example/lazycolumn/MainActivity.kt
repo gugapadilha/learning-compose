721b511b885fc4f978082341778af57e0e33a3e1
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-01-29 14:26:34 -0300

    paddins and space for lazy column

--- LazyColumn/app/src/main/java/com/example/lazycolumn/MainActivity.kt
+++ LazyColumn/app/src/main/java/com/example/lazycolumn/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.lazycolumn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.unit.dp
 import com.example.lazycolumn.repository.PersonRepository
 import com.example.lazycolumn.ui.CustomItem
 import com.example.lazycolumn.ui.theme.LazyColumnTheme
@@ -17,7 +20,10 @@ class MainActivity : ComponentActivity() {
                val personRepository = PersonRepository()
                 val getAllData = personRepository.getAllData()
                 
-                LazyColumn(){
+                LazyColumn(
+                    contentPadding = PaddingValues(all = 12.dp), //padding for all borders
+                    verticalArrangement = Arrangement.spacedBy(12.dp) // spacing between every Person
+                ){
                     items(items = getAllData){ person ->
                         CustomItem(person =  person)
                     }
