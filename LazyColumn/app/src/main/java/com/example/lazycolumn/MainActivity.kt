e10fa92690ff2e990b60f517eff9eb84a2d59795
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-01-29 14:23:16 -0300

    lazy column finished

--- LazyColumn/app/src/main/java/com/example/lazycolumn/MainActivity.kt
+++ LazyColumn/app/src/main/java/com/example/lazycolumn/MainActivity.kt
@@ -3,13 +3,10 @@ package com.example.lazycolumn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import com.example.lazycolumn.repository.PersonRepository
+import com.example.lazycolumn.ui.CustomItem
 import com.example.lazycolumn.ui.theme.LazyColumnTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,14 +14,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LazyColumnTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-
+               val personRepository = PersonRepository()
+                val getAllData = personRepository.getAllData()
+                
+                LazyColumn(){
+                    items(items = getAllData){ person ->
+                        CustomItem(person =  person)
+                    }
+                }
                 }
             }
         }
     }
-}
+
