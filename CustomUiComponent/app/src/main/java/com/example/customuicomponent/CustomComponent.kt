bdb0bde90a3d5734efc700782952cc6ef03c0827
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-01-31 14:27:50 -0300

    create extension function to make the background of component

--- CustomUiComponent/app/src/main/java/com/example/customuicomponent/CustomComponent.kt
+++ CustomUiComponent/app/src/main/java/com/example/customuicomponent/CustomComponent.kt
@@ -1,10 +1,15 @@
 package com.example.customuicomponent
 
+import android.graphics.Color
+import android.util.Size
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -20,10 +25,28 @@ fun CustomComponent(
             .size(canvasSize)
             .drawBehind {
                 val componentSize = size / 1.25f
+                backgroundIndicator()
             }
     ) {
 
     }
+}
+
+fun DrawScope.backgroundIndicator(
+    componentSize: Size,
+    indicatorColor: androidx.compose.ui.graphics.Color,
+    indicatorStrokeWidth: Float
+){
+    drawArc(
+        color = indicatorColor,
+        startAngle = 150f,
+        sweepAngle = 240f,
+        useCenter = false,
+        style = Stroke(
+            width = indicatorStrokeWidth,
+            cap = StrokeCap.Round
+        )
+    )
 }
 
 @Composable
