2baed4bdd92cfdda14036e9251d032cef75d5f86
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-02-07 15:03:22 -0300

    detail screen created

--- NavigationCompose/app/src/main/java/com/example/navigationcompose/DetailScreen.kt
+++ NavigationCompose/app/src/main/java/com/example/navigationcompose/DetailScreen.kt
@@ -7,19 +7,20 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun HomeScreen() {
+fun DetailScreen() {
     Box(
         modifier = Modifier
             .fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
         Text(
-            text = "Home",
-            color = MaterialTheme.colors.primary,
+            text = "Detail",
+            color = Color.Red,
             fontSize = MaterialTheme.typography.h3.fontSize,
             fontWeight = FontWeight.Bold
         )
@@ -28,6 +29,6 @@ fun HomeScreen() {
 
 @Composable
 @Preview(showBackground = true)
-fun HomeScreenPreview(){
-    HomeScreen()
+fun DetailScreenPreview(){
+    DetailScreen()
 }
