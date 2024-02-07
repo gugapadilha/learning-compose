6150676db97bc8e4b425a3e838b32d86401bf3d9
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-02-07 15:25:53 -0300

    added nav controller compose dependency

--- NavigationCompose/app/src/main/java/com/example/navigationcompose/MainActivity.kt
+++ NavigationCompose/app/src/main/java/com/example/navigationcompose/MainActivity.kt
@@ -10,13 +10,19 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.navigationcompose.ui.theme.NavigationComposeTheme
 
 class MainActivity : ComponentActivity() {
+
+    lateinit var navController: NavHostController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NavigationComposeTheme {
+
+                navController = rememberNavController()
 
             }
         }
