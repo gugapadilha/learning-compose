998ef72b08adcb6339ac28fad1d828421eda636b
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-02-07 15:00:37 -0300

    starting screen due navigation

---
+++ NavigationCompose/app/src/main/java/com/example/navigationcompose/HomeScreen.kt
@@ -0,0 +1,33 @@
+package com.example.navigationcompose
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Home",
+            color = MaterialTheme.colors.primary,
+            fontSize = MaterialTheme.typography.h3.fontSize,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+@Preview
+fun HomeScreenPreview(){
+    HomeScreen()
+}
