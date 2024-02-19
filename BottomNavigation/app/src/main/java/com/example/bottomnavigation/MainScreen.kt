699d7e20f2e1c041bfe58b573b40c3eab5537a30
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-02-19 16:36:17 -0300

    additem to allow navigation with destinations

--- BottomNavigation/app/src/main/java/com/example/bottomnavigation/MainScreen.kt
+++ BottomNavigation/app/src/main/java/com/example/bottomnavigation/MainScreen.kt
@@ -1,8 +1,14 @@
 package com.example.bottomnavigation
 
 import android.annotation.SuppressLint
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -10,8 +16,47 @@ import androidx.navigation.compose.rememberNavController
 fun MainScreen() {
     val navController = rememberNavController()
     Scaffold(
-        bottomBar = {}
+        bottomBar = { BottomBar(navController = navController)}
     ) {
-
+        BottomNavGraph(navController = navController)
     }
+}
+
+@Composable
+fun BottomBar(navController: NavHostController) {
+    val screens = listOf(
+        BottomBarScreen.Home,
+        BottomBarScreen.Profile,
+        BottomBarScreen.Settings
+    )
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    BottomNavigation{
+        screens.forEach { screen ->
+            AddItem(screen = screen, currentDestination = currentDestination, navController = navController)
+        }
+    }
+}
+
+@Composable
+fun RowScope.AddItem(
+    screen: BottomBarScreen,
+    currentDestination: NavDestination?,
+    navController: NavHostController
+) {
+    BottomNavigationItem(
+        label = {
+            Text(text = screen.title)
+        },
+        icon = {
+            Icon(imageVector = screen.icon , contentDescription = "Navigation Icon")
+        },
+        selected = currentDestination?.hierarchy?.any{
+            it.route == screen.route
+        } == true,
+        onClick = {
+            navController.navigate(screen.route)
+        }
+    )
 }
