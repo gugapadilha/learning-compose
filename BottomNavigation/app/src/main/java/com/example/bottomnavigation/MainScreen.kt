be5f7940e6d29edc611272614c1f102ec5d4dcde
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-02-19 16:26:48 -0300

    create bottom nav graph due routes

--- BottomNavigation/app/src/main/java/com/example/bottomnavigation/MainScreen.kt
+++ BottomNavigation/app/src/main/java/com/example/bottomnavigation/MainScreen.kt
@@ -1,8 +1,17 @@
 package com.example.bottomnavigation
 
+import android.annotation.SuppressLint
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun MainScreen() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = {}
+    ) {
 
+    }
 }
