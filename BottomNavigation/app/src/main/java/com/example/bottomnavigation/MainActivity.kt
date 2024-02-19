cd8d96e82d16dadca0b71d914be228e7055abae3
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-02-19 11:29:12 -0300

    added screens to make navigation possible later

--- BottomNavigation/app/src/main/java/com/example/bottomnavigation/MainActivity.kt
+++ BottomNavigation/app/src/main/java/com/example/bottomnavigation/MainActivity.kt
@@ -17,27 +17,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BottomNavigationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BottomNavigationTheme {
-        Greeting("Android")
+
     }
 }
