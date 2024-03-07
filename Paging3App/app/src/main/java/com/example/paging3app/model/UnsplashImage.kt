0e7a56b1084c9a7f2ecf4fc9a997350cf922da3b
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-03-07 14:34:44 -0300

    primary key added to id

--- Paging3App/app/src/main/java/com/example/paging3app/model/UnsplashImage.kt
+++ Paging3App/app/src/main/java/com/example/paging3app/model/UnsplashImage.kt
@@ -1,12 +1,17 @@
 package com.example.paging3app.model
 
+import androidx.room.Embedded
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.paging3app.util.Constants.UNSPLASH_IMAGE_TABLE
 
 @Entity(tableName = UNSPLASH_IMAGE_TABLE)
 data class UnsplashImage(
+    @PrimaryKey(autoGenerate = false)
     val id: String,
+    @Embedded
     val urls: Urls,
     val likes: Int,
+    @Embedded
     val user: User
 )
