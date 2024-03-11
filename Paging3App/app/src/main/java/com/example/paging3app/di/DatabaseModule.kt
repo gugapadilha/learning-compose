33dfe900dd38e57cbe35c05f5a75c706d06d8572
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-03-11 14:34:22 -0300

    implement database module

--- Paging3App/app/src/main/java/com/example/paging3app/di/DatabaseModule.kt
+++ Paging3App/app/src/main/java/com/example/paging3app/di/DatabaseModule.kt
@@ -1,11 +1,27 @@
 package com.example.paging3app.di
 
+import android.content.Context
+import androidx.room.Room
+import com.example.paging3app.data.local.UnsplashDatabase
+import com.example.paging3app.util.Constants.UNSPLASH_DATABASE
 import dagger.Module
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
+
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ): UnsplashDatabase {
+       return Room.databaseBuilder(
+           context,
+           UnsplashDatabase::class.java,
+           UNSPLASH_DATABASE
+       ).build()
+
+    }
 
 }
