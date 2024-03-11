530ff241394feccc0e93e690b39bb4316a9608e6
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-03-11 13:54:16 -0300

    add serializable

--- Paging3App/app/src/main/java/com/example/paging3app/model/UnsplashImage.kt
+++ Paging3App/app/src/main/java/com/example/paging3app/model/UnsplashImage.kt
@@ -1,10 +1,14 @@
+@file:Suppress("PLUGIN_IS_NOT_ENABLED")
+
 package com.example.paging3app.model
 
 import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.example.paging3app.util.Constants.UNSPLASH_IMAGE_TABLE
+import kotlinx.serialization.Serializable
 
+@Serializable
 @Entity(tableName = UNSPLASH_IMAGE_TABLE)
 data class UnsplashImage(
     @PrimaryKey(autoGenerate = false)
