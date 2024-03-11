5ae62b77e215a515eaee112b5cdc62270836820b
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-03-11 13:57:02 -0300

    setup dagger hilt lib

--- Paging3App/app/src/main/java/com/example/paging3app/MainActivity.kt
+++ Paging3App/app/src/main/java/com/example/paging3app/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.paging3app.ui.theme.Paging3AppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
