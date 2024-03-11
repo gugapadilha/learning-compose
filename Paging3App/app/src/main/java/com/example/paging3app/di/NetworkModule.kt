facbe06ce21471606e9096dcc33e672bfc0c8b5e
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-03-11 14:09:02 -0300

    create network module

--- Paging3App/app/src/main/java/com/example/paging3app/di/NetworkModule.kt
+++ Paging3App/app/src/main/java/com/example/paging3app/di/NetworkModule.kt
@@ -1,10 +1,44 @@
 package com.example.paging3app.di
 
+import com.example.paging3app.util.Constants.BASE_URL
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
+@OptIn(ExperimentalSerializationApi::class)
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+
+    }
+
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        val contentType = "application/json".toMediaType()
+        val json = Json {
+            ignoreUnknownKeys = true //Specifies whether encounters of unknown properties in the input JSON should be ignored instead of throwing SerializationException
+        }
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(json.asConverterFactory(contentType))
+            .build()
+    }
 }
--- Paging3App/app/src/main/java/com/example/paging3app/util/Constants.kt
+++ Paging3App/app/src/main/java/com/example/paging3app/util/Constants.kt
@@ -2,6 +2,7 @@ package com.example.paging3app.util
 
 object Constants {
 
+    const val BASE_URL = "https://api.unsplash.com"
     const val UNSPLASH_IMAGE_TABLE = "unsplash_image_table"
 
 }
