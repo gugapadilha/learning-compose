3a781edfc4bb6317617ce91dc8ffcea7b32ee6c0
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-03-12 15:14:32 -0300

    repository done

--- Paging3App/app/src/main/java/com/example/paging3app/data/repository/Repository.kt
+++ Paging3App/app/src/main/java/com/example/paging3app/data/repository/Repository.kt
@@ -1,16 +1,9 @@
 package com.example.paging3app.data.repository
 
-import androidx.compose.foundation.pager.HorizontalPager
 import androidx.paging.ExperimentalPagingApi
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.PagingData
 import com.example.paging3app.data.local.UnsplashDatabase
-import com.example.paging3app.data.paging.UnsplashRemoteMediator
 import com.example.paging3app.data.remote.UnsplashApi
-import com.example.paging3app.model.UnsplashImage
-import com.example.paging3app.util.Constants.ITEMS_PER_PAGE
-import kotlinx.coroutines.flow.Flow
+
 import javax.inject.Inject
 
 @ExperimentalPagingApi
