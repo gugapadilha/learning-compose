ad29cf5bbac2211b0f532dc2ffe8af43cd88f9f8
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-03-12 14:16:23 -0300

    load function created

--- Paging3App/app/src/main/java/com/example/paging3app/util/Constants.kt
+++ Paging3App/app/src/main/java/com/example/paging3app/util/Constants.kt
@@ -7,5 +7,6 @@ object Constants {
     const val UNSPLASH_DATABASE = "unsplash_database"
     const val UNSPLASH_IMAGE_TABLE = "unsplash_image_table"
     const val UNSPLASH_REMOTE_KEYS_TABLE = "unsplash_remote_key_table"
+    const val ITEMS_PER_PAGE = "items_per_page"
 
 }
