9540d0f32c2a181a288c78c453aa92dc8ed339d4
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-03-13 14:47:30 -0300

    create list of programming languages

--- LazyColumn/app/src/main/java/com/example/lazycolumn/ui/CustomItemListAnimation.kt
+++ LazyColumn/app/src/main/java/com/example/lazycolumn/ui/CustomItemListAnimation.kt
@@ -1,2 +1,23 @@
 package com.example.lazycolumn.ui
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+@Composable
+fun CustomItemListAnimation(){
+    var items by remember {
+        mutableStateOf(
+            listOf(
+                "Kotlin",
+                "Java",
+                "Python",
+                "Go",
+                "C++",
+                "C#"
+            )
+        )
+    }
+}
