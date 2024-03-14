d4c3cee043a888dcf5033696efa5c1da82a56b8e
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-03-14 14:17:04 -0300

    create row showing text and icon

--- AnimatedSelectableItem/app/src/main/java/com/example/animatedselectableitem/SelectableItem.kt
+++ AnimatedSelectableItem/app/src/main/java/com/example/animatedselectableitem/SelectableItem.kt
@@ -1,20 +1,32 @@
 package com.example.animatedselectableitem
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun SelectableItem(
-    modifier: Modifier = Modifier,
     selected: Boolean, //created to know when the object will be selected or not
     title: String,
     titleColor: Color = if (selected) MaterialTheme.colorScheme.primary
@@ -28,10 +40,50 @@ fun SelectableItem(
     borderColor: Color = if (selected) MaterialTheme.colorScheme.primary
                                  else MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f),
     icon: ImageVector = Icons.Default.CheckCircle,
+    borderShape: Shape = RoundedCornerShape(size = 10.dp),
     iconColor : Color = if (selected) MaterialTheme.colorScheme.primary
                         else MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f),
     onClick: () -> Unit
 ) {
 
-
+    Column(
+        modifier =
+        Modifier
+            .border(
+                width = borderWidth,
+                color = borderColor,
+                shape = borderShape
+            )
+            .clickable {
+                onClick()
+            }
+    ) {
+        Row(
+            modifier =
+            Modifier.padding(start = 12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                modifier = Modifier.weight(8f),
+                text = title,
+                style = TextStyle(
+                    color = titleColor,
+                    fontSize = titleSize,
+                    fontWeight = titleWeight
+                ),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            IconButton(
+                modifier = Modifier.weight(2f),
+                onClick = onClick
+            ) {
+                Icon(
+                    imageVector = icon ,
+                    contentDescription = "Selectable Item Icon",
+                    tint = iconColor
+                )
+            }
+        }
+    }
 }
