7e19b5d4c08ca29ec4723aa1bc22ea84599241b7
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-03-21 14:20:11 -0300

    setup navcontroller in main activity

--- NavParcelableObject/app/src/main/java/com/example/navparcelableobject/MainActivity.kt
+++ NavParcelableObject/app/src/main/java/com/example/navparcelableobject/MainActivity.kt
@@ -10,13 +10,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.navparcelableobject.navigation.SetupNavGraph
 import com.example.navparcelableobject.ui.theme.NavParcelableObjectTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            NavParcelableObjectTheme {
+                val navController = rememberNavController()
+                SetupNavGraph(navController = navController)
+            }
         }
     }
 }
