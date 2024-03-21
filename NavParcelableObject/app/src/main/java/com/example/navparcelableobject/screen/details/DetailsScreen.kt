6d29dee6c856be574ba2eaa17da2d1c4f9cb4b8e
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-03-21 14:14:13 -0300

    create detail screen as well

--- NavParcelableObject/app/src/main/java/com/example/navparcelableobject/screen/details/DetailsScreen.kt
+++ NavParcelableObject/app/src/main/java/com/example/navparcelableobject/screen/details/DetailsScreen.kt
@@ -1,8 +1,42 @@
 package com.example.navparcelableobject.screen.details
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun DetailScreen() {
-    
+fun DetailScreen(
+    navController: NavHostController
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .clickable {},
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Detail",
+            style = TextStyle(
+                fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+                fontWeight = FontWeight.Bold
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DetailScreenPreview() {
+    val navController = rememberNavController()
+    DetailScreen(navController = navController)
 }
