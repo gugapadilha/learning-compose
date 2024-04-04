0da3f3fefc6ad15ce3e86045af4b0dc57137e114
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-04-04 11:40:46 -0300

    add coil dependency

--- AdaptativeLayout/app/src/main/java/com/example/adaptativelayout/screen/home/HomeScreen.kt
+++ AdaptativeLayout/app/src/main/java/com/example/adaptativelayout/screen/home/HomeScreen.kt
@@ -1,11 +1,17 @@
 package com.example.adaptativelayout.screen.home
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
+import com.example.adaptativelayout.data.CustomData
 import com.example.adaptativelayout.screen.viewmodel.HomeViewModel
 
 @Composable
@@ -17,7 +23,7 @@ fun HomeScreen(homeViewModel: HomeViewModel = viewModel()) {
         contentPadding = PaddingValues(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        items(items = items, key = {it.id}) {
+        items(items = items, key = { it.id }) {
             CustomCard(data = it)
         }
     }
@@ -26,5 +32,18 @@ fun HomeScreen(homeViewModel: HomeViewModel = viewModel()) {
 
 @Composable
 fun CustomCard(data: CustomData) {
+    Row(
+        modifier = Modifier
+            .border(
+                width = 1.dp,
+                color = Color.LightGray
+            )
+    ) {
+        AsyncImage(
+            modifier = Modifier
+                .weight(1f),
 
+
+        )
+    }
 }
