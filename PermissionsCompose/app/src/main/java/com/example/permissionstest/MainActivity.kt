5b159586dbbba5b1eb97500d200830a25c6b1624
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-04-09 14:33:56 -0300

    permission to read contact and camera

--- PermissionsCompose/app/src/main/java/com/example/permissionstest/MainActivity.kt
+++ PermissionsCompose/app/src/main/java/com/example/permissionstest/MainActivity.kt
@@ -11,10 +11,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//single permission to only contacts
+              //single permission to only contacts
 //            RequestPermission(permission = Manifest.permission.READ_CONTACTS)
 
-            //multiple permissions to both contacts and camera
+            //multiple permissions to both contacts and camera (its better to allow permissions in separate situations to follow best practices
             RequestMultiplePermissions(
                 permissions = listOf(
                     Manifest.permission.READ_CONTACTS,
