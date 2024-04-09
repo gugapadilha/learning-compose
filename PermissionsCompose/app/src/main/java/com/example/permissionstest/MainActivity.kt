f71d2fcccd200be8afffbd4e1f2448fc49af1bfc
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-04-09 14:33:03 -0300

    permissions

--- PermissionsCompose/app/src/main/java/com/example/permissionstest/MainActivity.kt
+++ PermissionsCompose/app/src/main/java/com/example/permissionstest/MainActivity.kt
@@ -11,7 +11,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+//single permission to only contacts
 //            RequestPermission(permission = Manifest.permission.READ_CONTACTS)
+
+            //multiple permissions to both contacts and camera
             RequestMultiplePermissions(
                 permissions = listOf(
                     Manifest.permission.READ_CONTACTS,
