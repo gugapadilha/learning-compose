bfa6830754e1fce242f48218aed8efc69d33d649
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-04-16 14:17:12 -0300

    building MainScreen

--- MovingTextAnimation/app/src/main/java/com/example/movingtextanimation/MainScreen.kt
+++ MovingTextAnimation/app/src/main/java/com/example/movingtextanimation/MainScreen.kt
@@ -1,7 +1,24 @@
 package com.example.movingtextanimation
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ContentTransform
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.with
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @ExperimentalAnimationApi
@@ -9,5 +26,37 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 fun MainScreen(
     mainViewModel: MainViewModel = viewModel()
 ) {
+    val seconds by mainViewModel.seconds.collectAsState(initial = "00")
 
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        AnimatedContent(
+            targetState = seconds,
+            transitionSpec = {
+                addAnimation().using(
+                    SizeTransform(clip = true)
+                )
+            }
+
+        ) {targetCount ->
+            Text(
+                text = "$targetCount",
+                style = TextSt
+                )
+
+        }
+    }
+}
+
+@ExperimentalAnimationApi
+fun addAnimation(duration: Int = 800): ContentTransform{
+    return slideInVertically(animationSpec = tween(durationMillis = duration)) { height -> height } + fadeIn(
+        animationSpec = tween(durationMillis = duration)
+    )with slideOutVertically(animationSpec = tween(durationMillis = duration)) { height -> height } + fadeOut(
+        animationSpec = tween(durationMillis = duration)
+    )
 }
