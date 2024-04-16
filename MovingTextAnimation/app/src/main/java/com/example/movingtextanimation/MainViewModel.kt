726b31b5dcfa0baa9f93763d0cc0881b4a03c665
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-04-16 14:04:07 -0300

    create viewmodel logic

---
+++ MovingTextAnimation/app/src/main/java/com/example/movingtextanimation/MainViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.movingtextanimation
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+
+class MainViewModel : ViewModel() {
+
+    val seconds = (0..100)
+        .asSequence()
+        .asFlow()
+        .map {
+            if (it in 0..9) "0$it" else it
+        }
+        .onEach { delay(1000) }
+}
