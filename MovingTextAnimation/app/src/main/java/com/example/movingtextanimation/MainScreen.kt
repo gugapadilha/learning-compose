3a6dac1f4f93f129a87e98fab0ba57bbea3b7e33
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-04-16 14:07:25 -0300

    add viewmodel dependency

---
+++ MovingTextAnimation/app/src/main/java/com/example/movingtextanimation/MainScreen.kt
@@ -0,0 +1,13 @@
+package com.example.movingtextanimation
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@ExperimentalAnimationApi
+@Composable
+fun MainScreen(
+    mainViewModel: MainViewModel = viewModel()
+) {
+
+}
