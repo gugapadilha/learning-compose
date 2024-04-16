a52e3d3b6f6ce6411b621195dc8f42ba42a1181b
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-04-16 14:27:11 -0300

    counter complete

--- MovingTextAnimation/app/src/main/java/com/example/movingtextanimation/MainActivity.kt
+++ MovingTextAnimation/app/src/main/java/com/example/movingtextanimation/MainActivity.kt
@@ -3,21 +3,27 @@ package com.example.movingtextanimation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
 import com.example.movingtextanimation.ui.theme.MovingTextAnimationTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MovingTextAnimationTheme {
-
+                val mainViewModel : MainViewModel by viewModels()
+                MainScreen(mainViewModel)
             }
         }
     }
