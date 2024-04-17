bee93c225957e5009af6453fdaf24264bec359d9
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-04-17 14:09:36 -0300

    creating on start on create and on stop in MainActivity

--- StopwatchApplication/app/src/main/java/com/example/stopwatchapplication/MainActivity.kt
+++ StopwatchApplication/app/src/main/java/com/example/stopwatchapplication/MainActivity.kt
@@ -1,24 +1,68 @@
 package com.example.stopwatchapplication
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.stopwatchapplication.service.StopwatchService
 import com.example.stopwatchapplication.ui.theme.StopwatchApplicationTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@ExperimentalAnimationApi
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private var isBound by mutableStateOf(false)
+    private lateinit var stopwatchService: StopwatchService
+    private val connection = object : ServiceConnection {
+
+        override fun onServiceConnected(className: ComponentName, service: IBinder?) {
+            val binder = service as StopwatchService.StopwatchBinder
+            stopwatchService = binder.getService()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            isBound = false
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Intent(this, StopwatchService::class.java).also { intent ->
+            bindService(intent, connection, Context.BIND_AUTO_CREATE)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             StopwatchApplicationTheme {
-
+                if (isBound) {
+                    MainScreen(stopwatchService = stopwatchService)
+                }
             }
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unbindService(connection)
+        isBound = false
     }
 }
