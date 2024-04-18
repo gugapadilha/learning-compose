42cf40f7e5bb3493d1a43d689eb623d759158a08
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-04-18 14:00:02 -0300

    starting work in stopwatch service

--- StopwatchApplication/app/src/main/java/com/example/stopwatchapplication/service/StopwatchService.kt
+++ StopwatchApplication/app/src/main/java/com/example/stopwatchapplication/service/StopwatchService.kt
@@ -1,4 +1,17 @@
 package com.example.stopwatchapplication.service
 
-class StopwatchService {
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import androidx.compose.animation.ExperimentalAnimationApi
+import dagger.hilt.android.AndroidEntryPoint
+
+@ExperimentalAnimationApi
+@AndroidEntryPoint
+class StopwatchService : Service() {
+
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
 }
