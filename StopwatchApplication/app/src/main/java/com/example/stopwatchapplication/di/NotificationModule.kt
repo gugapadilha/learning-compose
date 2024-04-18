84f13ac977f408e8af42e72205129aed82b7f140
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-04-18 13:52:49 -0300

    create provide function to set functionalities to not bar

--- StopwatchApplication/app/src/main/java/com/example/stopwatchapplication/di/NotificationModule.kt
+++ StopwatchApplication/app/src/main/java/com/example/stopwatchapplication/di/NotificationModule.kt
@@ -1,8 +1,34 @@
 package com.example.stopwatchapplication.di
 
+import android.content.Context
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.core.app.NotificationCompat
+import com.example.stopwatch.service.ServiceHelper
+import com.example.stopwatchapplication.R
+import com.example.stopwatchapplication.util.Constants.NOTIFICATION_CHANNEL_ID
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ServiceComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ServiceScoped
 
 @InstallIn(ServiceComponent::class)
 object NotificationModule {
+
+    @OptIn(ExperimentalAnimationApi::class)
+    @ServiceScoped
+    @Provides
+    fun provideNotificationBuilder(
+        @ApplicationContext context: Context
+    ) : NotificationCompat.Builder {
+        return NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+            .setContentTitle("Stopwatch")
+            .setContentText("00:00:00")
+            .setSmallIcon(R.drawable.baseline_timer_24)
+            .setAutoCancel(false)
+            .setOngoing(true)
+            .addAction(0, "Stop", ServiceHelper.stopPendingIntent(context))
+            .addAction(0, "Cancel", ServiceHelper.cancelPendingIntent(context))
+            .setContentIntent(ServiceHelper.clickPendingIntent(context))
+    }
 }
