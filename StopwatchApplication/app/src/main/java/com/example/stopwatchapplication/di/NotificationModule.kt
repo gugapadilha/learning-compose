8f22847680059b412890c3c661888002ca0d31f7
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-04-18 13:27:09 -0300

    starting to wok in notification module hilt

---
+++ StopwatchApplication/app/src/main/java/com/example/stopwatchapplication/di/NotificationModule.kt
@@ -0,0 +1,8 @@
+package com.example.stopwatchapplication.di
+
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+
+@InstallIn(ServiceComponent::class)
+object NotificationModule {
+}
