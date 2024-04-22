5bae2a21fb09cb03e5535a956d56af68856dfc41
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-04-22 12:50:12 -0300

    button to success and error crated in main activity

--- TestingWithCompose/app/src/main/java/com/example/testingwithcompose/MainActivity.kt
+++ TestingWithCompose/app/src/main/java/com/example/testingwithcompose/MainActivity.kt
@@ -3,44 +3,49 @@ package com.example.testingwithcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.testingwithcompose.ui.theme.TestingWithComposeTheme
+import com.stevdzasan.messagebar.ContentWithMessageBar
+import com.stevdzasan.messagebar.rememberMessageBarState
+import java.lang.Exception
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TestingWithComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                val state = rememberMessageBarState()
+                ContentWithMessageBar(messageBarState = state) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Button(onClick =  {
+                            state.addError(exception = Exception("Internet Unavailable."))
+                        }) {
+                            Text(text = "Error")
+                        }
+                        Button(onClick = {
+                            state.addSuccess(message = "Successfully Updated. ")
+                        }) {
+                            Text(text = "Success")
+                        }
+
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TestingWithComposeTheme {
-        Greeting("Android")
-    }
-}
