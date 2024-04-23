3e8b8a12e897105557f1c60679c0fc508858bd37
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-04-23 10:53:05 -0300

    other text added in constants

--- TestingWithCompose/app/src/androidTest/java/com/example/testingwithcompose/TestTags.kt
+++ TestingWithCompose/app/src/androidTest/java/com/example/testingwithcompose/TestTags.kt
@@ -3,4 +3,5 @@ package com.example.testingwithcompose
 object TestTags {
     internal const val MESSAGE_BAR = "MessageBar"
     internal const val MESSAGE_BAR_TEXT = "MessageBarText"
+    internal const val COPY_BUTTON = "CopyButton"
 }
