0f0477120657fd8c7cf53e501d53fcf9585a7849
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-04-24 11:28:55 -0300

    animation complete with lottie

--- LottieAnimation/app/src/main/java/com/example/lottieanimation/MainActivity.kt
+++ LottieAnimation/app/src/main/java/com/example/lottieanimation/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -19,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
 import com.airbnb.lottie.compose.rememberLottieComposition
 
 class MainActivity : ComponentActivity() {
@@ -35,7 +37,18 @@ class MainActivity : ComponentActivity() {
 fun MainScreen() {
     val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.lottie_animation))
     var isPlaying by remember{ mutableStateOf(true) }
+    
+    val progress by animateLottieCompositionAsState(composition = composition, isPlaying)
 
+    LaunchedEffect(key1 = progress) {
+        if (progress == 0f){
+            isPlaying = true
+        }
+        if (progress == 1f){
+            isPlaying = false
+        }
+    }
+    
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -43,8 +56,9 @@ fun MainScreen() {
     ) {
         LottieAnimation(
             modifier = Modifier.size(400.dp),
-            composition = composition)
-        Button(onClick = { /*TODO*/ }) {
+            composition = composition,
+            progress = { progress })
+        Button(onClick = { isPlaying = true}) {
             Text(text = "Play Again")
         }
 
