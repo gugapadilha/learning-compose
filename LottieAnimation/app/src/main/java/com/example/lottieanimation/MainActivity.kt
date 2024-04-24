93289695374798f93cd508f0d3d00bb0e0d21eee
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-04-24 11:25:07 -0300

    added isPlaying variable to play again animation when click button

--- LottieAnimation/app/src/main/java/com/example/lottieanimation/MainActivity.kt
+++ LottieAnimation/app/src/main/java/com/example/lottieanimation/MainActivity.kt
@@ -6,12 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.rememberLottieComposition
@@ -29,13 +34,16 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen() {
     val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.lottie_animation))
+    var isPlaying by remember{ mutableStateOf(true) }
 
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        LottieAnimation(composition = composition)
+        LottieAnimation(
+            modifier = Modifier.size(400.dp),
+            composition = composition)
         Button(onClick = { /*TODO*/ }) {
             Text(text = "Play Again")
         }
