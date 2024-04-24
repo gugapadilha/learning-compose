c6a1f1431ef3ed2bd9105e6791494d2356632383
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-04-24 11:20:25 -0300

    added lottie animation composable function

--- LottieAnimation/app/src/main/java/com/example/lottieanimation/MainActivity.kt
+++ LottieAnimation/app/src/main/java/com/example/lottieanimation/MainActivity.kt
@@ -3,44 +3,42 @@ package com.example.lottieanimation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.lottieanimation.ui.theme.LottieAnimationTheme
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LottieAnimationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen() {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.lottie_animation))
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LottieAnimationTheme {
-        Greeting("Android")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        LottieAnimation(composition = composition)
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Play Again")
+        }
+
     }
 }
