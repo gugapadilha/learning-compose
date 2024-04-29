e5972bfb79b46c245795a0bed8c011bc2ef54bab
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-04-29 11:55:05 -0300

    finished ui to set design

--- FigmaLottieAnimation/app/src/main/java/com/example/figmalottieanimation/MainScreen.kt
+++ FigmaLottieAnimation/app/src/main/java/com/example/figmalottieanimation/MainScreen.kt
@@ -1,10 +1,24 @@
 package com.example.figmalottieanimation
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun MainScreen() {
@@ -12,6 +26,41 @@ fun MainScreen() {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.BottomCenter
     ) {
-
+        Column(
+            modifier = Modifier
+                .padding(bottom = 80.dp)
+                .padding(horizontal = 40.dp)
+        ) {
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                text = "Get Started",
+                color = Color.White,
+                fontSize = MaterialTheme.typography.displayLarge.fontSize,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                text = "Get Started",
+                color = Color.White,
+                fontSize = MaterialTheme.typography.displayLarge.fontSize,
+                fontWeight = FontWeight.Normal
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(54.dp),
+                onClick = {},
+                //colors = ButtonDefaults.buttonColors(containerColor = )
+                shape = CircleShape
+            ) {
+                Text(
+                    text = "Recipes, Now!",
+                    color = Color.Black,
+                    )
+            }
+        }
     }
 }
