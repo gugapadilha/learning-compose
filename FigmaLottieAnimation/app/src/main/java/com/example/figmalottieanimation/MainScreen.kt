b934afdc081c0a8be135955ead15a31a97ae5c77
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-04-29 14:03:10 -0300

    trying to add lottie animation that i create in figma to rotate image

--- FigmaLottieAnimation/app/src/main/java/com/example/figmalottieanimation/MainScreen.kt
+++ FigmaLottieAnimation/app/src/main/java/com/example/figmalottieanimation/MainScreen.kt
@@ -34,7 +34,7 @@ import com.airbnb.lottie.compose.rememberLottieComposition
 
 @Composable
 fun MainScreen() {
-    val composition = rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.background2))
+    val composition = rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.android))
     val progress by animateLottieCompositionAsState(composition = composition.value, iterations = LottieConstants.IterateForever)
 
     val infiniteTransition = rememberInfiniteTransition()
