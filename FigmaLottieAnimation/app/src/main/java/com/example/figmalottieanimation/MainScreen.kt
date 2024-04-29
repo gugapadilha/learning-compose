68ee33e020cf49a7044b96ca160e0abc39936e28
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-04-29 11:23:54 -0300

    added lottie animation that i created in figma with aninx

---
+++ FigmaLottieAnimation/app/src/main/java/com/example/figmalottieanimation/MainScreen.kt
@@ -0,0 +1,17 @@
+package com.example.figmalottieanimation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+
+    }
+}
