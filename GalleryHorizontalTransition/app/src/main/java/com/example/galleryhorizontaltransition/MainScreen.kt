021060784d471a64275e2f6b9489502594cdc866
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-04-30 11:42:59 -0300

    images scrolling and getting fullscreen when is in front

--- GalleryHorizontalTransition/app/src/main/java/com/example/galleryhorizontaltransition/MainScreen.kt
+++ GalleryHorizontalTransition/app/src/main/java/com/example/galleryhorizontaltransition/MainScreen.kt
@@ -1,6 +1,8 @@
 package com.example.galleryhorizontaltransition
 
 import android.annotation.SuppressLint
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,7 +13,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -37,13 +42,22 @@ fun MainScreen() {
     Scaffold(
         modifier = Modifier.padding(vertical = 48.dp)
     ) {
-        HorizontalPager(state = pagerState) {
+        HorizontalPager(state = pagerState) { index ->
+            val pageOffset = (pagerState.currentPage - index) + pagerState.currentPageOffsetFraction
+            val imageSize by animateFloatAsState(
+                targetValue = if(pageOffset != 0.0f) 0.75f else 1f,
+                animationSpec = tween(durationMillis = 300), label = ""
+            )
             AsyncImage(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(16.dp)
-                    .clip(RoundedCornerShape(16.dp)),
-                model = ImageRequest.Builder(LocalContext.current).data(images[it]).build(),
+                    .clip(RoundedCornerShape(16.dp))
+                    .graphicsLayer {
+                                   scaleX = imageSize
+                                    scaleY = imageSize
+                    },
+                model = ImageRequest.Builder(LocalContext.current).data(images[index]).build(),
                 contentDescription = "Image",
                 contentScale = ContentScale.Crop
             )
