649dfe87b9bb26905df9f45a2f192493b22d3906
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-04-30 11:30:57 -0300

    images scrolling just fine

--- GalleryHorizontalTransition/app/src/main/java/com/example/galleryhorizontaltransition/MainScreen.kt
+++ GalleryHorizontalTransition/app/src/main/java/com/example/galleryhorizontaltransition/MainScreen.kt
@@ -1,8 +1,25 @@
 package com.example.galleryhorizontaltransition
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 
+@OptIn(ExperimentalFoundationApi::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MainScreen() {
 
@@ -11,9 +28,25 @@ fun MainScreen() {
             R.drawable.image1,
             R.drawable.image2,
             R.drawable.image3,
-            R.drawable.image4,
-            R.drawable.image5,
-            R.drawable.image6
+            R.drawable.image4
         )
+    }
+
+    val pagerState = rememberPagerState(pageCount = { images.size })
+
+    Scaffold(
+        modifier = Modifier.padding(vertical = 48.dp)
+    ) {
+        HorizontalPager(state = pagerState) {
+            AsyncImage(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+                    .clip(RoundedCornerShape(16.dp)),
+                model = ImageRequest.Builder(LocalContext.current).data(images[it]).build(),
+                contentDescription = "Image",
+                contentScale = ContentScale.Crop
+            )
+        }
     }
 }
