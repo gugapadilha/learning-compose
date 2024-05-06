c26cc1dd4a127d3d86082e5e927d2c51bdef0bd8
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-05-06 11:34:45 -0300

    creating ui to display text with border around it

--- AnimatedBorder/app/src/main/java/com/example/animatedborder/MainScreen.kt
+++ AnimatedBorder/app/src/main/java/com/example/animatedborder/MainScreen.kt
@@ -6,14 +6,20 @@ import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawWithContent
@@ -22,9 +28,43 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
+
+@Composable
+fun MainScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.surface),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        AnimatedBorderCard(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 24.dp),
+            shape = RoundedCornerShape(8.dp),
+            borderWidth = 1.dp,
+            gradient = Brush.linearGradient(listOf(Color.Magenta, Color.Cyan)),
+            onCardClick = {}
+        ){
+            Column(
+                modifier = Modifier.padding(all = 24.dp)){
+                Text(
+                    fontSize = MaterialTheme.typography.displaySmall.fontSize,
+                    fontWeight = FontWeight.Bold,
+                    text = "Welcome"
+                )
+            }
+
+        }
+
+    }
+    
+}
 
 @Composable
     fun AnimatedBorderCard(
