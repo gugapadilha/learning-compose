60e080fababa66aee957a897e9ff7de9fb215a2e
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-05-13 14:15:58 -0300

    starting list screen ui and logic

--- SharedElementTransition/app/src/main/java/com/example/sharedelementtransition/screen/ListScreen.kt
+++ SharedElementTransition/app/src/main/java/com/example/sharedelementtransition/screen/ListScreen.kt
@@ -1,14 +1,32 @@
 package com.example.sharedelementtransition.screen
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.dp
 import com.example.sharedelementtransition.model.mountains
 import com.skydoves.orbital.Orbital
+import com.skydoves.orbital.animateBounds
+import com.skydoves.orbital.rememberMovableContentOf
 
 @Composable
 fun ListScreen() {
@@ -21,8 +39,38 @@ fun ListScreen() {
             items(
                 items = mountains,
                 key = {it.title}
-            ) {
-                mountain ->
+            ) { mountain ->
+                var expanded by rememberSaveable { mutableStateOf(false) }
+                AnimatedVisibility(
+                    remember {
+                         MutableTransitionState(false) }
+                        .apply { targetState = true },
+                    enter = fadeIn(tween(durationMillis = 300)),
+                    exit = fadeOut(tween(durationMillis = 300))
+                ) {
+                    Orbital(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickableWithoutRipple(
+                                interactionSource = MutableInteractionSource(),
+                                onClick = {expanded = !expanded}
+                            )
+                    ) {
+                        val text = rememberMovableContentOf {
+                            Column(
+                                modifier = Modifier
+                                    .padding(vertical = 10.dp)
+                                    .padding(horizontal = if (expanded) 20.dp else 10.dp)
+                                    .animateBounds(
+                                        sizeAnimationSpec = tween(durationMillis = 300),
+                                        positionAnimationSpec = tween(durationMillis = 300)
+                                    )
+                            ) {
+
+                            }
+                        }
+                    }
+                }
             }
         }
     }
