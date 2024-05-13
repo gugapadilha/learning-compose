2fae2c475d14577edadf8ab084e0cf79a3e9d9ed
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-05-13 13:53:22 -0300

    create mountain data class

---
+++ SharedElementTransition/app/src/main/java/com/example/sharedelementtransition/model/Mountain.kt
@@ -0,0 +1,26 @@
+package com.example.sharedelementtransition.model
+
+data class Mountain(
+    val title: String,
+    val description: String,
+    val image: String
+)
+
+val mountains = listOf(
+    Mountain(
+        "Gray and Brown Mountain",
+        "In the heart of nature's majestic canvas, the \"Gray and Brown Mountain\" emerges",
+        "https://shorturl.at/hjHV0"
+    ),
+    Mountain(
+        "Mountains Covered in Snow",
+        "A pristine world unfolds beneath the title \"Mountains Covered in Snow\" emerges",
+        "https://shorturl.at/lvF02"
+    ),
+    Mountain(
+        "Black and Teal Mountain",
+        "Amidst the wild expanse, the \"Black and Teal Mountain\" emerges ",
+        "https://shorturl.at/aDS17"
+    )
+
+)
