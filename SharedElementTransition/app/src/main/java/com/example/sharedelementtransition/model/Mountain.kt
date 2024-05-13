6ea573fdcf174d073cec2299870fa361fc66d334
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-05-13 16:44:31 -0300

    application finished

--- SharedElementTransition/app/src/main/java/com/example/sharedelementtransition/model/Mountain.kt
+++ SharedElementTransition/app/src/main/java/com/example/sharedelementtransition/model/Mountain.kt
@@ -9,18 +9,18 @@ data class Mountain(
 val mountains = listOf(
     Mountain(
         "Gray and Brown Mountain",
-        "In the heart of nature's majestic canvas, the \"Gray and Brown Mountain\" emerges",
+        "In the heart of nature's majestic canvas, the \"Gray and Brown Mountain\" emerges sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
         "https://shorturl.at/hjHV0"
     ),
     Mountain(
         "Mountains Covered in Snow",
-        "A pristine world unfolds beneath the title \"Mountains Covered in Snow\" emerges",
-        "https://shorturl.at/lvF02"
+        "A pristine world unfolds beneath the title \"Mountains Covered in Snow\" emerges sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+        "https://shorturl.at/CKU09"
     ),
     Mountain(
         "Black and Teal Mountain",
-        "Amidst the wild expanse, the \"Black and Teal Mountain\" emerges ",
-        "https://shorturl.at/aDS17"
+        "Amidst the wild expanse, the \"Black and Teal Mountain\" emerges sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+        "https://shorturl.at/ruyE2"
     )
 
 )
--- SharedElementTransition/app/src/main/java/com/example/sharedelementtransition/screen/ListScreen.kt
+++ SharedElementTransition/app/src/main/java/com/example/sharedelementtransition/screen/ListScreen.kt
@@ -1,5 +1,6 @@
 package com.example.sharedelementtransition.screen
 
+import android.annotation.SuppressLint
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.animation.core.tween
@@ -9,15 +10,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
@@ -25,14 +30,21 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.sharedelementtransition.model.mountains
 import com.skydoves.orbital.Orbital
 import com.skydoves.orbital.animateBounds
 import com.skydoves.orbital.rememberMovableContentOf
 
+@SuppressLint("UnrememberedMutableInteractionSource")
 @Composable
 fun ListScreen() {
     Orbital {
@@ -89,7 +101,48 @@ fun ListScreen() {
                                 )
                             }
                         }
-
+                        val image = rememberMovableContentOf {
+                            AsyncImage(
+                                modifier = Modifier
+                                    .padding(all = 10.dp)
+                                    .animateBounds(
+                                        modifier = if (expanded){
+                                            Modifier.fillMaxWidth()
+                                        } else {
+                                            Modifier.size(100.dp)
+                                        },
+                                        sizeAnimationSpec = tween(durationMillis = 300),
+                                        positionAnimationSpec = tween(durationMillis = 300),
+                                    )
+                                    .clip(RoundedCornerShape(10.dp)),
+                                model = ImageRequest.Builder(LocalContext.current)
+                                    .data(mountain.image)
+                                    .crossfade(true)
+                                    .build(),
+                                contentDescription = "Mountain Image",
+                                contentScale = ContentScale.Crop
+                            )
+                        }
+                        if (expanded) {
+                            Column(
+                                modifier = Modifier
+                                    .padding(all = 10.dp)
+                                    .clip(RoundedCornerShape(size = 10.dp))
+                                    .background(
+                                        MaterialTheme.colorScheme.surfaceColorAtElevation(
+                                            elevation = 10.dp
+                                        )
+                                    )
+                            ) {
+                                image()
+                                text()
+                            }
+                        } else {
+                            Row {
+                                image()
+                                text()
+                            }
+                        }
                     }
                 }
             }
@@ -97,3 +150,18 @@ fun ListScreen() {
     }
 
 }
+
+fun Modifier.clickableWithoutRipple(
+    interactionSource: MutableInteractionSource,
+    onClick: () -> Unit
+) = composed(
+    factory = {
+        this.then(
+            Modifier.clickable(
+                interactionSource = interactionSource,
+                indication = null,
+                onClick = { onClick()}
+            )
+        )
+    }
+)
